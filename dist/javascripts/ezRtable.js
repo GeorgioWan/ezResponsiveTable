3158e043c93ec1d7586aa31a4cafd0417cc717db
GeorgioWan <georgio.wan@gmail.com>
2016-02-22 12:41:43 +0800

    Added files via upload

--- dist/javascripts/ezRtable.js
+++ dist/javascripts/ezRtable.js
@@ -5,7 +5,7 @@
 function ezRtable(){
   if($('.table-ezr').length){
     var headertext = [],
-    headers = document.querySelectorAll(".table-ezr .th-erz"),
+    headers = document.querySelectorAll(".table-ezr .th-ezr"),
     tablebody = document.querySelector(".table-ezr tbody");
     
     for(var i = 0; i < headers.length; i++) {
@@ -15,7 +15,7 @@ function ezRtable(){
     for (var i = 0, row; row = tablebody.rows[i]; i++) {
       var ht = 0;
       for (var j = 0, col; col = row.cells[j]; j++) {
-        if ( col.className == "td-erz" )
+        if ( col.className == "td-ezr" )
           col.setAttribute("data-th", headertext[ht++]);
       } 
     }
